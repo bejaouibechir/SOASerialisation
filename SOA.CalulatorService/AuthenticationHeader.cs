e0bad1a204d9c262f800fabe6ca1808681705ca0
bechirbejaoui <me780411@gmail.com>
2023-02-07 10:07:10 +0100

    Ajout de header au servce web asmx

---
+++ SOA.CalulatorService/AuthenticationHeader.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Web.Services.Protocols;
+
+namespace SOA.CalulatorService
+{
+    public class AuthenticationHeader :SoapHeader
+    {
+        public string Login { get; set; }
+        public string Pasword { get; set; }
+
+        public bool Valid
+        {
+            get
+            {
+                return GetUserInfo(Login,Pasword);
+            }
+        }
+
+        private bool GetUserInfo(string login,string pass)
+        {
+            if (login.Equals("bechir".ToLower()) &&
+                  pass.Equals("pass".ToLower()))
+               return true;
+            else return false;
+        }
+    }
+}
--- SOA.CalulatorService/CalculatorService.asmx.cs
+++ SOA.CalulatorService/CalculatorService.asmx.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Services.Protocols;
 
 namespace SOA.CalulatorService
 {
@@ -17,6 +20,8 @@ namespace SOA.CalulatorService
     public class CalculatorService //: System.Web.Services.WebService
     {
 
+        public AuthenticationHeader header;
+        
         [WebMethod]
         public double Add(int a, int b)
         {
@@ -26,6 +31,33 @@ namespace SOA.CalulatorService
         public double Substract(int a, int b)
         {
             return a - b;
+        }
+        [WebMethod]
+        [SoapHeader("header",Direction = SoapHeaderDirection.In)]
+        public double SubstractVIP(int a, int b)
+        {
+            try
+            {
+                if (header.Valid == true)
+                {
+                    header.DidUnderstand = true;
+                }
+                else
+                {
+                    header.DidUnderstand = false;
+                    throw new Exception("Utilisateur n'est pas authentifié");
+                }
+
+                return a - b;
+            }
+            catch (Exception caught)
+            {
+                DateTime time = DateTime.Now;
+                string message = $"Error time: {time.Hour}:{time.Minute}:{time.Second} error message: {caught.Message}"; 
+                EventLog.WriteEntry("Application", message,
+                    EventLogEntryType.Error);
+                return int.MaxValue;
+            }
         }
         [WebMethod]
         public double Mulbtiply(int a, int b)
